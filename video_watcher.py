2e5af63e976a14d70e90726a59ca050bd738efd4
Matheus Borges Teixeira <matheus.borgest@gmail.com>
2021-04-10 14:00:28 -0300

    first prototype

--- video_watcher.py
+++ video_watcher.py
@@ -1,15 +1,17 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
+from tinydb import TinyDB
 import pickle
 import time
-import random
+import os
 
 BS_DELAY = 6
 LOAD_DELAY = 6
 
 
 def get_seconds_from_timestamp(text):
-	values = text.split(':')
+	values = text.split(':')[::-1]
 	total_seconds = 0
 	for i in range(len(values)):
 		total_seconds += int(values[i]) * (60 ** i)
@@ -36,7 +38,18 @@ def get_frontpage_view_count(text):
 class VideoWatcher:
 
 	def __init__(self, first_video_url, session_name, time_limit):
-		self.driver = webdriver.Chrome(executable_path='./chromedriver')
+		options = webdriver.ChromeOptions()
+		options.add_extension('./adblock.crx')
+		self.driver = webdriver.Chrome(executable_path='./chromedriver', options=options)
+
+		try:
+			os.makedirs(session_name)
+		except:
+			None
+		db_path = './' + session_name + '/db.json'
+		file = open(db_path, 'w')
+		file.close()
+		self.db = TinyDB(db_path)
 
 		self.first_video_url = first_video_url
 		self.session_name = session_name
@@ -50,6 +63,21 @@ class VideoWatcher:
 		cookies = pickle.load(open('/session_cookies/' + self.session_name + '.pkl', 'rb'))
 		for cookie in cookies:
 			self.driver.add_cookie(cookie)
+
+	def save_data(self, watched, recommended):
+		recommended_dict = dict()
+		for video in recommended:
+			recommended_dict.update({'title': video.title, 'channel': video.channel, 'view_amount': video.view_amount, 'url': video.url})
+
+		self.db.insert({
+			'watched': {
+				'title': watched.title,
+				'channel': watched.channel,
+				'view_amount': watched.view_amount,
+				'url': watched.url
+			},
+			'recommended': recommended_dict
+		})
 
 	def run(self):
 		input()
@@ -66,8 +94,8 @@ class VideoWatcher:
 
 			time.sleep(LOAD_DELAY)
 
-			video_player = self.driver.find_element_by_xpath('//*[@id="movie_player"]/div[5]/button')
 			try:
+				video_player = self.driver.find_element_by_xpath('//*[@id="movie_player"]/div[4]/button')
 				video_player.click()
 			except:
 				None
@@ -85,9 +113,11 @@ class VideoWatcher:
 			video_duration_txt = bs.find('span', {'class': 'ytp-time-duration'}).text
 			video_duration = get_seconds_from_timestamp(video_duration_txt)
 
-			#time.sleep(video_duration - BS_DELAY)
+			watched_video = Video(video_title, video_channel_name, video_view_count, next_video_url)
 
-			self.driver.get('https://www.youtube.com')
+			time.sleep(video_duration - BS_DELAY)
+
+			self.driver.get('https://www.youtube.com/')
 			time.sleep(BS_DELAY)
 			bs = BeautifulSoup(self.driver.page_source, 'lxml')
 
@@ -108,20 +138,23 @@ class VideoWatcher:
 
 				channel = meta.find('a', {'class': 'yt-simple-endpoint style-scope yt-formatted-string', 'spellcheck': 'false'}).text
 
-				frontpage_video_info.append(RecomendedVideo(title, view_amount, channel, url))
+				frontpage_video_info.append(Video(title, channel, view_amount, url))
 
 			video_title_words = []
+			channel_name_param = ''
 			if use_old_word_set:
 				video_title_words = old_word_set
+				channel_name_param = old_channel_name
 			else:
-				video_title_words = video_title.split()
+				video_title_words = watched_video.title.split()
+				channel_name_param = watched_video.channel
 
 			flag = True
 			count = 0
 			while flag and count < len(frontpage_video_info):
 				r_video = frontpage_video_info[count]
 				for word in r_video.title:
-					if word in video_title_words:
+					if word in video_title_words or r_video.channel == channel_name_param:
 						next_video_url = r_video.url
 						flag = False
 						break
@@ -129,16 +162,17 @@ class VideoWatcher:
 
 			if flag:
 				old_word_set = video_title_words
+				old_channel_name = watched_video.channel
 				next_video_url = frontpage_video_info[0].url
 			use_old_word_set = flag
 
+			self.save_data(watched_video, frontpage_video_info)
 
 
+class Video:
 
-class RecomendedVideo:
-
-	def __init__(self, title, view_amount, channel, url):
+	def __init__(self, title, channel, view_amount, url):
 		self.title = title
-		self.view_amount = view_amount
 		self.channel = channel
+		self.view_amount = view_amount
 		self.url = url
